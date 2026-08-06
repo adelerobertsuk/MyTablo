68e6bca02217232a55d053061199a33092d642c3
Adele Roberts <adele.roberts@me.com>
2026-08-06 14:04:12 +0100

    Initial Commit

---
+++ ReadingTable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReadingTable
+//
+//  Created by Adele Roberts on 06/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReadingTable/ReadingTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingTableApp.swift
+//  ReadingTable
+//
+//  Created by Adele Roberts on 06/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReadingTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
